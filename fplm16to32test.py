5a0c22733e5d610babe82c3c0a0f4b053288ae47
QuentinKoeninger <qkoenin@okstate.edu>
2026-03-31 20:05:34 -0500

    Updated fplm16to32test.py to be more readable and remove redundant code

--- fplm16to32test.py
+++ fplm16to32test.py
@@ -1,53 +1,5 @@
 import torch
 
-def generate_vector_file(mul1, mul2, prod):
-    a = mul1.to(torch.float16).view(torch.uint16)
-    b = mul2.to(torch.float16).view(torch.uint16)
-    c = prod.to(torch.float16).view(torch.uint16)
-
-    a1 = mul1.to(torch.float16)
-    b1 = mul2.to(torch.float16)
-    c1 = prod.to(torch.float16) 
-
-
-    with open("C:/Users/qkoen/Desktop/GitHub/fplm2_configurable/SV/fplm2_testvectors.tv", "w", encoding="utf-8") as file:
-
-        for w1, w2, w3, w4, w5, w6 in zip(a, b, c, a1, b1, c1):
-            file.write(f"{w1.item():4x}_{w2.item():4x}_{w3.item():4x}\t\t// {w4.item():<8} * {w5.item():<8} = {w6.item():<8}\n")
-
-        file.close()
-
-
-
-# -----------------------------
-# FPLM-2: Approximate floating-point multiplication (Method-2)
-# -----------------------------
-def approx_mul_fplm2_16(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-    sign = torch.sign(a) * torch.sign(b)
-    aa = a.abs().clamp_min(torch.finfo(torch.float16).tiny)
-    bb = b.abs().clamp_min(torch.finfo(torch.float16).tiny)
-    m_a, e_a = torch.frexp(aa)
-    m_b, e_b = torch.frexp(bb)
-
-    M_a = 2.0 * m_a
-    M_b = 2.0 * m_b
-    x_a = M_a - 1.0
-    x_b = M_b - 1.0
-
-    bXa = torch.where(x_a < 0.5, x_a, 0.5 * (1.0 + x_a))
-    bXb = torch.where(x_b < 0.5, x_b, 0.5 * (1.0 + x_b))
-
-    s = bXa + bXb
-    carry = (s >= 1.0).to(e_a.dtype)
-    e_p = (e_a + e_b - 2).to(torch.int32) + carry.to(torch.int32)
-
-    Xp = torch.where(s < 1.0, 1.0 + s,
-             torch.where(s < 1.5, s,
-             torch.where(s < 1.75, s - 0.25,
-                         s - 0.125)))
-
-    return sign * torch.ldexp(Xp, e_p)
-
 # -----------------------------
 # FPLM-2: Approximate floating-point multiplication (Method-2) 32
 # -----------------------------
@@ -78,7 +30,7 @@ def approx_mul_fplm2_32(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     return sign * torch.ldexp(Xp, e_p)
 
 # -----------------------------
-# FPLM-2: Approximate floating-point multiplication (Method-2)
+# FPLM-2: Approximate floating-point multiplication (Method-2) 32-bit configurable
 # -----------------------------
 def approx_mul_fplm2_32_conf(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     sign = torch.sign(a) * torch.sign(b)
@@ -130,37 +82,34 @@ def approx_mul_fplm2_32_conf(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     ep1 = (ea1 + eb1).to(torch.int32) + torch.where(ep2i > (2**(5)), (2**(5)), 0).to(torch.int32)
     # End sum exponents separate
 
+    # Start Compute Anti-log (only done on most significant bits)
     s0 = torch.where(s0i < 1.0, 1.0 + s0i,
              torch.where(s0i < 1.5, s0i,
              torch.where(s0i < 1.75, s0i - 0.25,
                          s0i - 0.125)))
+    # End Compute Anti-log
     
+    # Add pieces back together
     Xp = s0 + s1 + s2
     e_p = ep1 + ep2
 
     return sign * torch.ldexp(Xp, e_p)
 
 if __name__ == "__main__":
-    a = torch.normal(mean=0.0, std=150.0, size=(1000,1))
-    b = torch.normal(mean=0.0, std=150.0, size=(1000,1))
-
-    a16=a.to(dtype=torch.float16)
-    b16=b.to(dtype=torch.float16)
-
-    a32 = a.to(dtype=torch.float32)
-    b32 = b.to(dtype=torch.float32)
+    a = torch.normal(mean=0.0, std=150.0, size=(1000,1), dtype=torch.float32)
+    b = torch.normal(mean=0.0, std=150.0, size=(1000,1), dtype=torch.float32)
 
     print("\nGetting Results...")
-    fplm2_result_32_conf = approx_mul_fplm2_32_conf(a32, b32)
-    fplm2_result_32 = approx_mul_fplm2_32(a32, b32)
-    comp = torch.eq(fplm2_result_32_conf, fplm2_result_32)
+    fplm2_result_32_conf = approx_mul_fplm2_32_conf(a, b)
+    fplm2_result_32 = approx_mul_fplm2_32(a, b)
+    comp_res = torch.eq(fplm2_result_32_conf, fplm2_result_32)
 
 #    print(fplm2_result_32)
 #    print(fplm2_result_32_conf)
 #    print(comp)
 
     err_sum = 0
-    for x in comp:
+    for x in comp_res:
         if not x:
             err_sum += 1
     
@@ -169,6 +118,6 @@ if __name__ == "__main__":
 
 #    print("Generating .tv file...")
 #    generate_vector_file(a, b, fplm2_result)
-#
-#    print("Done!\n")
+    
+    print("Done!\n")
 
