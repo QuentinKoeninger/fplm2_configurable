e0a41dde515dbfb0c6b043d2d9fffd3aa2607d9e
QuentinKoeninger <qkoenin@okstate.edu>
2026-04-25 14:55:14 -0500

    fplm16to32 updates

--- fplm16to32test.py
+++ fplm16to32test.py
@@ -1,4 +1,6 @@
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
 
 # -----------------------------
 # FPLM-2: Approximate floating-point multiplication (Method-2) 32
@@ -95,9 +97,72 @@ def approx_mul_fplm2_32_conf(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
 
     return sign * torch.ldexp(Xp, e_p)
 
+
+def approx_anti_log2(s):
+    return torch.where(s < 1.0, 1.0 + s,
+             torch.where(s < 1.5, 2*s,
+             torch.where(s < 1.75, 2*(s - 0.25),
+                         2*(s - 0.125))))
+
+def approx_anti_log2_improved(s):
+    return torch.where(s < 1.0, 
+             torch.where((s < 0.25) | (s >= 0.75), s+1,
+                         s+0.9375),
+             torch.where(s < 1.5, 
+             torch.where(s<1.25, 2*(s-0.03125),
+                         2*(s-0.0625)),
+             torch.where(s < 1.75, 2*(s - 0.125),
+                         2*(s - 0.0625))))
+
+def approx_anti_log2_workImproved(s):
+    return torch.where(s < 1.0, 
+             torch.where((s>=0.25) & (s<0.5),0.875+s,
+                         1.0 + s),
+             torch.where(s < 1.5, 
+             torch.where(s<1.25, 
+             torch.where(s>=1.125, 2*(s-0.0625), 
+                         2*s), 
+                         2*(s-0.125)),
+             torch.where((s < 1.75),
+             torch.where(s >= 1.625, 
+                         2*(s - 0.0625), 2*(s)),
+             torch.where(s >= 1.875, 2*(s-0.0625),
+                         2*(s - 0.125)))))
+
+def anti_log2(s):
+    return 2**s
+
 if __name__ == "__main__":
-    a = torch.normal(mean=0.0, std=150.0, size=(1000,1), dtype=torch.float32)
-    b = torch.normal(mean=0.0, std=150.0, size=(1000,1), dtype=torch.float32)
+#    a = torch.normal(mean=0.0, std=150.0, size=(1000,1), dtype=torch.float32)
+#    b = torch.normal(mean=0.0, std=150.0, size=(1000,1), dtype=torch.float32)
+
+    NUM = 5000
+
+    a = torch.tensor([0.75])
+    b = torch.tensor([1.2])
+
+    sn = torch.linspace(0.0,2.0,NUM)
+
+    X0 = anti_log2(sn)
+    X1 = approx_anti_log2(sn)
+    X2 = approx_anti_log2_improved(sn)
+    X3 = approx_anti_log2_workImproved(sn)
+
+    X1_err_ser = X0-X1
+    X2_err_ser = X0-X2
+    X3_err_ser = X0-X3
+
+    X1_err = torch.sum(X1_err_ser)
+    X2_err = torch.sum(X2_err_ser)
+    X3_err = torch.sum(X3_err_ser)
+
+    X1_err_abs = torch.sum(torch.abs(X1_err_ser)) / NUM * 100
+    X2_err_abs = torch.sum(torch.abs(X2_err_ser)) / NUM * 100
+    X3_err_abs = torch.sum(torch.abs(X3_err_ser)) / NUM * 100
+
+    X1_err_max = torch.max(torch.abs(X1_err_ser))
+    X2_err_max = torch.max(torch.abs(X2_err_ser))
+    X3_err_max = torch.max(torch.abs(X3_err_ser))
 
     print("\nGetting Results...")
     fplm2_result_32_conf = approx_mul_fplm2_32_conf(a, b)
@@ -114,7 +179,58 @@ if __name__ == "__main__":
             err_sum += 1
     
     print("Number of errors = " + str(err_sum))
+    print(a)
+    print(b)
+    print(fplm2_result_32)
+    print(a*b)
 
+    fig1, ax1 = plt.subplots(2, 1, figsize=(7,8))
+    ax1[0].plot(sn.numpy(), X1.numpy(), lw=2)
+    ax1[0].plot(sn.numpy(), X0.numpy(), linestyle='--', lw=2) 
+    ax1[0].grid(True)
+    ax1[1].plot(sn.numpy(), X2.numpy(), lw=2, c='purple')
+    ax1[1].plot(sn.numpy(), X0.numpy(), linestyle='--', lw=2, c='orange') 
+    ax1[1].grid(True)
+
+    fig2, ax2 = plt.subplots(2, 1, figsize=(7,8))
+    ax2[0].plot(sn.numpy(), X1.numpy(), lw=2)
+    ax2[0].plot(sn.numpy(), X0.numpy(), linestyle='--', lw=2)
+    ax2[0].grid(True) 
+    ax2[1].plot(sn.numpy(), X3.numpy(), lw=2, c='green')
+    ax2[1].plot(sn.numpy(), X0.numpy(), linestyle='--', lw=2, c='orange')
+    ax2[1].grid(True)
+
+    fig3, ax3 = plt.subplots(3, 1, figsize=(5,8))
+    ax3[0].plot(sn.numpy(), X1_err_ser.numpy(), lw=2)
+    ax3[0].grid(True)
+    ax3[0].set_ylim(-0.5, 0.5)
+    ax3[1].plot(sn.numpy(), X2_err_ser.numpy(), lw=2, c='purple')
+    ax3[1].grid(True)
+    ax3[1].set_ylim(-0.5, 0.5)
+    ax3[2].plot(sn.numpy(), X3_err_ser.numpy(), lw=2, c='green')
+    ax3[2].grid(True)
+    ax3[2].set_ylim(-0.5, 0.5)
+    
+
+    print("Total Error")
+
+    print(X1_err)
+    print(X2_err)
+    print(X3_err)
+
+    print("Absolute Error, %")
+
+    print(X1_err_abs)
+    print(X2_err_abs)
+    print(X3_err_abs)
+
+    print("Maximum Error")
+
+    print(X1_err_max)
+    print(X2_err_max)
+    print(X3_err_max)
+
+    plt.show()
 
 #    print("Generating .tv file...")
 #    generate_vector_file(a, b, fplm2_result)
